6ef959b272962b1c91b84d5f2e1d4f9fc17f0ce6
pitap <58887305+pitap@users.noreply.github.com>
2021-05-26 17:41:24 +0400

    add lab 5

---
+++ java lab5/src/com/company/Series/SynchronizedSeriesable.java
@@ -0,0 +1,62 @@
+package com.company.Series;
+
+import java.io.OutputStream;
+import java.io.Writer;
+
+public class SynchronizedSeriesable implements Seriesable {
+    private final Seriesable s;
+
+    public SynchronizedSeriesable(Seriesable s) {
+        this.s = s;
+    }
+
+    @Override
+    public synchronized int getNumOfEls() {
+        return s.getNumOfEls();
+    }
+
+    @Override
+    public synchronized String getTitle() {
+        return s.getTitle();
+    }
+
+    @Override
+    public synchronized void setTitle(String title) {
+        s.setTitle(title);
+    }
+
+    @Override
+    public synchronized int getNumOfStartPages() {
+        return s.getNumOfStartPages();
+    }
+
+    @Override
+    public synchronized void setNumOfStartPages(int num) {
+        s.setNumOfStartPages(num);
+    }
+
+    @Override
+    public synchronized int getNumOfPagesOfEl(int index) {
+        return s.getNumOfPagesOfEl(index);
+    }
+
+    @Override
+    public synchronized int getSumOfPagesWithoutStart() {
+        return s.getSumOfPagesWithoutStart();
+    }
+
+    @Override
+    public synchronized void setNumOfPagesOfEl(int index, int num) {
+        s.setNumOfPagesOfEl(index, num);
+    }
+
+    @Override
+    public synchronized void output(OutputStream out) {
+        s.output(out);
+    }
+
+    @Override
+    public synchronized void write(Writer out) {
+        s.write(out);
+    }
+}
